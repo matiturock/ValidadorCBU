2b540461381fd3546a92ab0bebbfe370788bfe1e
Matías Almirón <matiturock@gmail.com>
2023-03-01 01:36:01 -0300

    hot fix

--- ValidadorCBU/Program.cs
+++ ValidadorCBU/Program.cs
@@ -7,7 +7,6 @@ class Program
         Console.WriteLine("VALIDAR CBU");
         const string CBU = "2850015740095451234988";
 
-        var validadorCbu = new ValidadorCbu(CBU);
 
         Console.WriteLine(validadorCbu);
         Console.WriteLine(validadorCbu.bancoActual);
